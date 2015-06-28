0b64c6d2b04555565cf93c6d732c572e9c65cce4
Bao Linh <masusapu@gmail.com>
2015-06-28 19:26:06 +0900

    Add 1st user: Rinrin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(username: 'Rinrin', email: 'masusapu@gmail.com', admin: true)
