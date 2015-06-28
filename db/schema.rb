2856158ff44c959ad07beb6e95d1a75f14de1f2d
Bao Linh <masusapu@gmail.com>
2015-06-28 22:44:39 +0900

    Add Items scaffold + test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628095131) do
+ActiveRecord::Schema.define(version: 20150628123055) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
