fcc5b5be5b0e1eeb3dac1184883c3515953acbc5
Bao Linh <masusapu@gmail.com>
2015-08-03 05:22:08 +0900

    Add category + item models

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :categories
   root 'static_pages#home'
   get '/shop' => 'static_pages#shop'
   get '/blog' => 'static_pages#blog'
-  resources :items
   get '/users' => 'users#index'
+
+  resources :categories do
+    resources :items
+  end
+
+  resources :items
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create(username: 'Rinrin', email: 'masusapu@gmail.com', admin: true)
+Category.create(name: "Origami")
+Category.create(name: "Needle felting")
+Item.create(name: "Bookmark", image: "http://www.inkprint.in/media/catalog/product/cache/1/image/265x/9df78eab33525d08d6e5fb8d27136e95/b/o/bookmark.jpg", description: "A paper bookmark", price: 1.0, category_id: 1)
+Item.create(name: "Kitten", image: "https://s-media-cache-ak0.pinimg.com/236x/e1/59/f7/e159f77dfbc64623ac74187491e50b46.jpg", description: "A felted kitten", price: 10.0, category_id: 2)
